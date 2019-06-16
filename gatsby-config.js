1f2be1c443dc35a32b19ffa5bff71e32a9a26a0b
Khaled Garbaya <khaled@contentful.com>
2019-06-16 16:03:05 +0200

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Harissan'CO`,
+    description: `Popup Brunch Series in Berlin introducing Tunisian cuisine with a modern twist all along the Summer`,
+    author: `@harissanco`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,8 +27,20 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        tailwind: true,
+        purgeOnly: ['src/css/style.css']
+      }
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.HCO_SPACE_ID,
+        accessToken: process.env.HCO_ACCESS_TOKEN
+      }
+    },
   ],
 }
