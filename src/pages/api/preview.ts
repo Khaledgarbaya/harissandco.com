f55f17d7f7024cddbd5a72fbf921c7f39a1361e5
Khaled Garbaya <khaledgarbaya@gmail.com>
2023-11-01 13:09:07 +0100

    fix preview

--- src/pages/api/preview.ts
+++ src/pages/api/preview.ts
@@ -1,8 +1,7 @@
-import { createPreviewHandler } from '@uniformdev/canvas-next';
+import { createPreviewHandler } from "@uniformdev/canvas-next"
 
 // Preview Mode, more info https://nextjs.org/docs/advanced-features/preview-mode
 export default createPreviewHandler({
-  playgroundPath: '/playground',
-  secret: () => process.env.UNIFORM_PREVIEW_SECRET || 'hello-world',
-  playgroundPath: '/playground',
-});
+  playgroundPath: "/playground",
+  secret: () => process.env.UNIFORM_PREVIEW_SECRET || "hello-world",
+})
